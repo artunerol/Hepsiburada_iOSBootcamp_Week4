fba3509a0b4a805a8318930d856184de8731e8cd
Artun <artun.erol@yahoo.com>
2021-10-18 17:59:04 +0300

    Test

--- Week4/ViewController.swift
+++ Week4/ViewController.swift
@@ -12,9 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Test")
-        print("Dev")
-        print("FirstScenes")
     }
 
 
