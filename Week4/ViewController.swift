c7a12dd1e51be2347f3738c495411ef24bbcf810
Artun <artun.erol@yahoo.com>
2021-10-18 17:35:04 +0300

    Initial Commit

---
+++ Week4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week4
+//
+//  Created by Artun Erol on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
